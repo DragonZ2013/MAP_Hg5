5d80a695c9d9e8c351c9fe831399b8aaea1b14e3
DragonZ <drag.popescu@yahoo.com>
2021-12-09 09:53:30 +0200

    More Course Tests and Teacher Tests

--- src/test/java/Main/Controller/ControllerTest.java
+++ src/test/java/Main/Controller/ControllerTest.java
@@ -39,16 +39,16 @@ class ControllerTest {
         List<Integer> listInt7 = List.of(2);
         List<Integer> listInt8 = Arrays.asList(2,3);
         Course course1 = new Course("course1",1,10, listInt1,30,1);
-        Course course2 = new Course("course2",1,25, listInt2,25,2);
-        Course course3 = new Course("course3",2,30, listInt3,20,3);
+        Course course2 = new Course("course2",1,25, listInt2,45,2);//25
+        Course course3 = new Course("course3",2,30, listInt3,10,3);//20
         Course course4 = new Course("course4",3,20, listInt4,15,4);
         Teacher teacher1 = new Teacher ("teacherfirstname1","teacherlastname1",1);
         Teacher teacher2 = new Teacher ("teacherfirstname2","teacherlastname2",2);
         Teacher teacher3 = new Teacher ("teacherfirstname3","teacherlastname3",3);
-        Student student1 = new Student("studentfirstname1","studentlastname1",1,55,listInt5);
-        Student student2 = new Student("studentfirstname2","studentlastname2",2,65,listInt6);
-        Student student3 = new Student("studentfirstname3","studentlastname3",3,25,listInt7);
-        Student student4 = new Student("studentfirstname4","studentlastname4",4,45,listInt8);
+        Student student1 = new Student("studentfirstname1","studentlastname1",1,75,listInt5);
+        Student student2 = new Student("studentfirstname2","studentlastname2",2,55,listInt6);
+        Student student3 = new Student("studentfirstname3","studentlastname3",3,45,listInt7);
+        Student student4 = new Student("studentfirstname4","studentlastname4",4,65,listInt8);
         List<Course> listCourse1 = Arrays.asList(course1,course2,course3,course4);
         List<Teacher> listTeacher1 = Arrays.asList(teacher1,teacher2,teacher3);
         List<Student> listStudent1 = Arrays.asList(student1,student2,student3,student4);
@@ -80,11 +80,11 @@ class ControllerTest {
         assertEquals(3,controller.listCourses().get(0).getStudentsEnrolled().size());
 
         assertEquals(2,controller.listCourses().get(1).getCourseId());
-        assertEquals(25,controller.listCourses().get(1).getCredits());
+        assertEquals(45,controller.listCourses().get(1).getCredits());
         assertEquals(3,controller.listCourses().get(1).getStudentsEnrolled().size());
 
         assertEquals(3,controller.listCourses().get(2).getCourseId());
-        assertEquals(20,controller.listCourses().get(2).getCredits());
+        assertEquals(10,controller.listCourses().get(2).getCredits());
         assertEquals(2,controller.listCourses().get(2).getStudentsEnrolled().size());
 
         assertEquals(4,controller.listCourses().get(3).getCourseId());
@@ -99,5 +99,35 @@ class ControllerTest {
 
         MissingIdException thrownMissingIdException3 = assertThrows(MissingIdException.class,() -> controller.updateCourse("tempname2",4,10,30,1));
         assertEquals("Teacher with given Id doesn't exist",thrownMissingIdException3.getMessage());
+
+        assertEquals(4,controller.filterCourses(9).size());
+        assertEquals(3,controller.filterCourses(14).size());
+        assertEquals(2,controller.filterCourses(29).size());
+        assertEquals(1,controller.filterCourses(44).size());
+        assertEquals(0,controller.filterCourses(46).size());
+
+        assertEquals(3,controller.sortCourses().get(0).getCourseId());
+        assertEquals(4,controller.sortCourses().get(1).getCourseId());
+        assertEquals(1,controller.sortCourses().get(2).getCourseId());
+        assertEquals(2,controller.sortCourses().get(3).getCourseId());
     }
+
+    @org.junit.jupiter.api.Test
+    void testTeacher() throws SQLException, MissingIdException {
+        assertEquals(3,controller.listTeachers().size());
+
+        assertEquals(1,controller.listTeachers().get(0).getTeacherId());
+
+        assertEquals(2,controller.listTeachers().get(1).getTeacherId());
+
+        assertEquals(3,controller.listTeachers().get(2).getTeacherId());
+
+        MissingIdException thrownMissingIdException1 = assertThrows(MissingIdException.class,()->controller.deleteTeacher(4));
+        assertEquals("Teacher with given Id doesn't exist",thrownMissingIdException1.getMessage());
+
+        MissingIdException thrownMissingIdException2 = assertThrows(MissingIdException.class,()->controller.updateTeacher("teachertestfirstname","teachertestlastname",4));
+        assertEquals("Teacher with given Id doesn't exist",thrownMissingIdException2.getMessage());
+
+    }
+
 }
